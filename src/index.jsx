9078cde301d268ec002ec574484469091a9b048a
Daniel Somtoochi Dav-Emmanuel <74049793+royalpriniel@users.noreply.github.com>
2026-05-17 18:33:10 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client';
-import {LifespanEstimator} from './lifespanEstimator';
+import LifespanEstimator from './lifespanEstimator';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<LifespanEstimator />);
