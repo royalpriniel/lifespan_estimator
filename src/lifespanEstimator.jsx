518b5f5aec05d62869b93310c48936bde05fa58c
Daniel Somtoochi Dav-Emmanuel <74049793+royalpriniel@users.noreply.github.com>
2026-05-18 20:21:48 +0100

    Update lifespanEstimator.jsx

--- src/lifespanEstimator.jsx
+++ src/lifespanEstimator.jsx
@@ -26,7 +26,7 @@ const LifespanEstimator = () => {
 
     setResults(stats);
   };
-     const inputStyle = {{ borderRadius: '0.5rem', height: '2.5rem'}};
+     const inputStyle = { borderRadius: '0.5rem', height: '2.5rem'};
 
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif', maxWidth: '500px' }}>
